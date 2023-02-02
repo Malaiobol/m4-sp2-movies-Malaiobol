f78676d790f0b0b1cdff90b647c993410fe549f6
Edmilson <krakadianbill@gmail.com>
2023-02-02 12:45:02 -0300

    feat: initial logic && initial mask

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express, { Application } from "express";
+import { startDatabase } from "./database";
 
 const app: Application = express();
 app.use(express.json());
 
 const PORT: number = 3000;
 const runningMsg: string = `Server is running on ${PORT}!`; 
-app.listen(PORT, ()=>{
-    
+app.listen(PORT, async()=>{
+    await startDatabase();
     console.log(runningMsg);
 });
