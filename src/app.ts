95b7a4247ae0ba935ad1b2dea30281573a73f58b
Edmilson <krakadianbill@gmail.com>
2023-02-02 13:26:24 -0300

    feat: initial routes && initial logic to lisMovies

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express, { Application } from "express";
 import { startDatabase } from "./database";
+import { createMovie, deleteMovie, listMovies, updateMovie } from "./logic";
 
 const app: Application = express();
 app.use(express.json());
 
+app.post("/movie", createMovie);
+app.get("/movie", listMovies);
+app.patch("/movie", updateMovie);
+app.delete("/movie", deleteMovie);
+
 const PORT: number = 3000;
 const runningMsg: string = `Server is running on ${PORT}!`; 
+
 app.listen(PORT, async()=>{
     await startDatabase();
     console.log(runningMsg);
+
 });
--- src/logic.ts
+++ src/logic.ts
@@ -1,17 +1,29 @@
 import { Request, Response } from "express";
+import { client } from "./database";
 
 const createMovie = async (req: Request, resp: Response): Promise<Response> =>{
     return resp.status(201).json(req)
-}
+};
 
 const listMovies = async (req: Request, resp: Response): Promise<Response> =>{
-    return resp.status(201).json(req)
-}
+
+    const query: string = `
+        SELECT
+            *
+        FROM
+            movies_list;
+    `
+    const queryResult = await client.query(query);
+    
+    return resp.status(201).json(queryResult);
+};
 
 const updateMovie = async (req: Request, resp: Response): Promise<Response> =>{
     return resp.status(201).json();
-}
+};
 
 const deleteMovie = async (req: Request, resp: Response): Promise<Response> =>{
     return resp.status(204);
-}
+};
+
+export { createMovie, listMovies, updateMovie, deleteMovie };
